00d51f56bfd84e863c5e99464fa941add309a82b
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-26 03:39:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ function produceDrivingRange(blocks) {
     let end = parseInt(endingBlock)
      let distanceToTravel = Math.abs(end - start)
     let difference = blockRange - distanceToTravel
-    
+      
+        if (difference > 0) {
+      return `within range by ${difference}`
+    } else {
+      return `${Math.abs(difference)} blocks out of range`
+    }
+  }
 }
