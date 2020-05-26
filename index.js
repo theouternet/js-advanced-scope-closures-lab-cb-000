dfeda2fe254102961ffa47a1ba139397732f897b
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-26 03:38:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function produceDrivingRange(blocks) {
     return function(startingBlock, endingBlock) {
     let start = parseInt(startingBlock)
     let end = parseInt(endingBlock)
-     let distanceToTravel = Math.abs(end - start);
-    let difference = blockRange - distanceToTravel;
+     let distanceToTravel = Math.abs(end - start)
+    let difference = blockRange - distanceToTravel
+    
 }
